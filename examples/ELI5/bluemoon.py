8a57259056c436e45aecf4e4e1c070897de50b17
owo441 <82258733+owo441@users.noreply.github.com>
2023-11-07 03:49:16 +0900

    Update bluemoon.py

--- examples/ELI5/bluemoon.py
+++ examples/ELI5/bluemoon.py
@@ -12,30 +12,18 @@ PARSER_NAME = "bluemoon"
 
 
 class bluemoon(DataParser):
-    def __init__(self, file_path: str, output_path: str, target_lang: str="ko",
-                 max_example_per_thread=400, large_chunks_threshold=20000):
-        super().__init__(file_path, output_path,
-                         parser_name=PARSER_NAME,
-                         do_translate=True,
-                         target_lang=target_lang,
-                         max_example_per_thread=max_example_per_thread,
-                         large_chunks_threshold=large_chunks_threshold)
-        self.max_ctxs = 5
-        # The data config to be validated to check if self implement "convert" function is correct or not
-        self.target_config = BaseConfig
+    # __init__ 메소드는 생략하겠습니다.
 
-        # The data fields to be translated (The fields belong to BaseConfig)
-        self.target_fields = ['question_text', 'orig_answer_texts']
+    def read(self) -> None:
+        with open(self.file_path, encoding='utf-8') as jfile:
+            json_data = json.load(jfile)
 
-    # Read function must assign data that has been read to self.data_read
-def read(self) -> None:
-    with open(self.file_path, encoding='utf-8') as jfile:
-        json_data = json.load(jfile)
-
-    self.data_read = json_data[0]
-    return None
+        self.data_read = json_data[0]
+        return None
 
     def convert(self) -> None:
+        # convert 메소드의 내용은 생략하였습니다.
+        return None
 
         lfqa_prefixs = [
             "\n\n Here are some relevant documents, which may or may not be applicable to the previous question. If you use this information, please indicate 'Based on the provided documents':\n",
