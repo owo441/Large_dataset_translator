e2751c0193bbc18825d230929c0ab52985a7b3ae
owo441 <82258733+owo441@users.noreply.github.com>
2023-11-07 05:04:37 +0900

    Update bluemoon.py

--- examples/ELI5/bluemoon.py
+++ examples/ELI5/bluemoon.py
@@ -7,9 +7,7 @@ from tqdm.auto import tqdm
 from configs import BaseConfig
 from translator import DataParser
 
-
 PARSER_NAME = "bluemoon"
-
 
 class bluemoon(DataParser):
     def __init__(self, file_path: str, output_path: str, target_lang: str="ko",
@@ -21,42 +19,30 @@ class bluemoon(DataParser):
                          max_example_per_thread=max_example_per_thread,
                          large_chunks_threshold=large_chunks_threshold)
         self.max_ctxs = 5
-        # The data config to be validated to check if self implement "convert" function is correct or not
         self.target_config = BaseConfig
-
-        # The data fields to be translated (The fields belong to BaseConfig)
         self.target_fields = ['question_text', 'orig_answer_texts']
 
-    # Read function must assign data that has been read to self.data_read
-def read(self) -> None:
-    with open(self.file_path, encoding='utf-8') as jfile:
-        json_data = json.load(jfile)  # Changed from 'json.loads' to 'json.load'
-
-    self.data_read = json_data[0]  # This line may also need to be modified depending on the structure of your JSON file.
-    return None
+    def read(self) -> None:
+        with open(self.file_path, encoding='utf-8') as jfile:
+            json_data = json.load(jfile)
+        self.data_read = json_data[0]
+        return None
 
     def convert(self) -> None:
         super().convert()
-
         data_converted = []
         for data in tqdm(self.data_read, desc="Converting data"):
             data_dict = {}
-
             data_dict['qas_id'] = data['question_id']
-
             docs = [ctx[0] for ctx in data['ctxs'][:self.max_ctxs]]
             data_dict['question_text'] = data['question']
             for doc in docs:
                 data_dict['question_text'] += doc + "\n\n"
-
             data_dict['orig_answer_texts'] = data['answers'][0] if data['answers'] else None
             data_dict['answer_lengths'] = None
             data_converted.append(data_dict)
-
         self.converted_data = data_converted
-
         return None
-
 
 if __name__ == '__main__':
     bluemoon_parser = bluemoon(r"examples/ELI5/bluemoon.train.json",
