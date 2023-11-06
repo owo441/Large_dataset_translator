b1d2b5be697d2448eb13be2febf6da03ff35d152
owo441 <82258733+owo441@users.noreply.github.com>
2023-11-07 04:41:26 +0900

    Update bluemoon.py

--- examples/ELI5/bluemoon.py
+++ examples/ELI5/bluemoon.py
@@ -60,7 +60,7 @@ class bluemoon(DataParser):
 
 
 if __name__ == '__main__':
-    bluemoon_parser = bluemoon(r"examples/bluemoon/bluemoon.train.json",
+    bluemoon_parser = bluemoon(r"examples/ELI5/bluemoon.train.json",
                               r"examples/bluemoon",
                               max_example_per_thread=100,
                               large_chunks_threshold=1000,
